af7db359d401dd95b61d9c76ea029750a75a2bff
AndreaChiappini <117998815+AndreaChiappini@users.noreply.github.com>
2022-12-14 11:53:30 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,11 @@
+//
+// Created by Andrea Chiappini on 14/12/22.
+//
+
+#ifndef ESERCITAZIONE8_LIB_H
+#define ESERCITAZIONE8_LIB_H
+
+float funzione (float numeri[], int dimensione);
+
+#endif //ESERCITAZIONE8_LIB_H
 
