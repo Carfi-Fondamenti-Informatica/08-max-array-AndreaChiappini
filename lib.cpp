6d960d09db3ad3d159224e17e58e736f7109a97c
AndreaChiappini <117998815+AndreaChiappini@users.noreply.github.com>
2022-12-14 11:53:06 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+//
+// Created by Andrea Chiappini on 14/12/22.
+//
 
+#include "lib.h"
+
+float funzione(float numeri[], int dimensione){
+    float massimo= numeri[0];
+    for (int i=0;i < dimensione;i++) {
+        if (massimo < numeri[i]) {
+            massimo = numeri[i];
+        }
+    }
+    return massimo;
+}
